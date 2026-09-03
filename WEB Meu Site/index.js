e58919317d0ddeb3f6d35b9aa63a59169f6991fd
MateusPicinin <picininmateus@gmail.com>
2026-09-03 11:42:20 -0300

    Botões Dos Apps prontos

--- WEB Meu Site/index.js
+++ WEB Meu Site/index.js
@@ -1,5 +1,25 @@
 const botao = document.getElementById('BtnBatalhaNaval');
 
 botao.addEventListener('click', function() {
-    window.open('https://www.youtube.com/', '_blank');
+    window.open('https://github.com/MateusPicinin/Projects/tree/main/Python%20Batalha%20Naval', '_blank');
+});
+
+const botaoGithub = document.getElementById('github');
+
+botaoGithub.addEventListener('click', function() {
+    window.open('https://github.com/MateusPicinin', '_blank');
+});
+
+
+const botaoLinkedin = document.getElementById('linkedin');
+
+botaoLinkedin.addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/mateus-augusto-picinin-5082683b8/', '_blank');
+});
+
+
+const botaoInstagram = document.getElementById('instagram');
+
+botaoInstagram.addEventListener('click', function() {
+    window.open('https://www.instagram.com/mts_picinin/', '_blank');
 });
